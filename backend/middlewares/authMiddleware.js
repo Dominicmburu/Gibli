197010fb875450fdd795c704469bc4e29f8435fb
EvanMworia <evannji99@gmail.com>
2025-08-03 23:58:23 +0300

    Added Token Verificatiion middleware

---
+++ backend/middlewares/authMiddleware.js
@@ -0,0 +1,26 @@
+// middleware/authMiddleware.js
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+dotenv.config({ path: path.join(__dirname, '../.env') });
+
+export function authenticateToken(req, res, next) {
+	const authHeader = req.headers['authorization'];
+	const token = authHeader && authHeader.split(' ')[1];
+
+	if (!token) {
+		return res.status(401).json({ message: 'Access token missing' });
+	}
+
+	jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+		if (err) {
+			return res.status(403).json({ message: 'Invalid or expired token' });
+		}
+
+		req.user = user;
+		next();
+	});
+}
